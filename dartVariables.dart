3fcb70e81583fcfc4bb1ffd0e4e292420a421065
fehzitu <fehzitujsdev@gmail.com>
2025-11-01 20:48:03 -0300

    first commit

---
+++ dartVariables.dart
@@ -0,0 +1,28 @@
+// { STEP 1: TYPES OF VARIABLES }
+
+/*
+In dart, we have 10 main types of declarable variables:
+
+Example of each:
+*/
+
+int ten = 10; // Only integrer numbers
+double value = 22.67; // Only decimal numbers
+num anyoneAbove = 10; // Decimal & integrer
+String name = 'Arthur'; // Only string
+List users = ['Arthur', 'Felipe', 'Luana']; // Only array
+Map object = {'nome': 'Ana', 'idade': 20}; // Only key-value
+Set mySet = {1, 2, 3, 4, 5}; // Set without repetition
+final singleAssignment = 10; // Only a single assignment
+const myConstant = 'Hi'; // A common constante like js const
+dynamic = 'Dart'; // It accepts any type of payment and its value can be changed.
+
+/*
+Example of each:
+*/
+
+var name = "Ana"; // Type assigned: String
+int age = 18; // Explicit type: int
+dynamic data = "Text"; // You can change the typing
+final city = "Curitiba"; // You cannot change the typing
+const pi = 3.1415; // Fixed constant
