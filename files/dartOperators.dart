b2e2662215dff043621caff7bc3b44e825d39912
fehzitu <fehzitujsdev@gmail.com>
2025-11-03 19:57:15 -0300

    add dart operators topic

---
+++ files/dartOperators.dart
@@ -0,0 +1,31 @@
+/*
+{ STEP 1: ARITHMETIC OPERATORS }
+
+In Dart we have 7 arithmetic's operators, see an example of each:
+*/
+
+int addition = a + b; // + is addition operator
+int subtraction = a - b; // - is subtraction operator
+int multiplication = a * b; // * is multiplication operator
+int division = a / b; // / is division operator
+int rest = a % b; // % is rest of the division operator
+int increment = a++; // % is increment operator
+int decrement = a--; // % is decrement operator
+
+/*
+{ STEP 2: ASSIGNMENT OPERATORS }
+
+Assignment operators, they are simple to understand and use. Follow the examples with their explanations.
+
+Example of each:
+*/
+
+int val = 10; // int receives 10
+int addEqual += 10; // same as addEqual = addEqual + 10
+int subEqual -= 10; // same as subEqual = subEqual - 10
+
+/*
+{ STEP 3: LOGICAL OPERATORS }
+
+// CODE HERE //
+*/
--- files/dartVariables.dart
+++ files/dartVariables.dart
@@ -43,3 +43,68 @@ var camelCase = 'Im an camelCase variable';
 var snake_case = 'Im an snake_case variable';
 var PascalCase = 'Im an PascalCase variable';
 var kebab_case = 'Im an kebab-case variable';
+
+/*
+{ STEP 3: DYNAMIC TYPING OF VARIABLES }
+
+That being said, keep in mind that Dart is softly typed and not dynamic by nature, but it can be dynamic when needed.
+
+Example:
+*/
+
+var x = 10; // int type only
+x = "text"; // compilation error (It can't change type at runtime)
+
+dynamic y = 10;
+y = "text"; // It can change type at runtime
+}
+
+/*
+That being said, keep in mind that Dart is static by default, even when using 'var', and its attributes do not change at runtime
+
+On the other hand, the 'dynamic' type can be used, which makes it dynamic similar to Python, Javascript, or Ruby
+*/
+
+/*
+{ STEP 4: HOISTING }
+No, Unlike languages like Javascript, Dart doesn't use hoisting, as it's checked at compile time (before execution). Therefore, if something is used before being declared, it will return an error.
+
+Example:
+*/
+
+void main() {
+  print(a); // Error: 'a' was not declared
+  int a = 10;
+}
+
+/*
+The correct way to do it:
+
+Example:
+*/
+
+void main() {
+  int a = 10;
+  print(a);
+}
+
+/*
+{ STEP 5: FUNCTION HOISTING }
+
+You need to declare the function before calling it, or at least ensure that the compiler knows about it (for example, if it's declared later but in the same file, the compiler already reads everything beforehand).
+
+Example:
+*/
+
+void main() {
+  hello(); // It works
+}
+
+void hello() {
+  print("Hi, Dart!");
+}
+
+/*
+It works normally here because the Dart compiler simply analyzes the entire file before compiling, so it already knows hello().
+Because they are in the same file and the compiler has already read the entire file before executing it.
+*/
